77c91401654df06dfff38b2cdf610c5d47b69ec6
khamis_dossary <khamis.dossary@gmail.com>
2023-10-14 16:50:10 +0300

    printed the documentation

--- labFunctions1.py
+++ labFunctions1.py
@@ -1,15 +1,14 @@
 def function1(n):
     '''
-    this function that takes 1 parameter of type int,
-    then it prints out the result 
-    formatted like the following patter 
-    (if we give it 5 for example):
-    5 4 3 2 1   
-    4 3 2 1   
-    3 2 1   
-    2 1   
-    1   
-    '''
+this function that takes 1 parameter of type int,
+then it prints out the result 
+formatted like the following patter 
+(if we give it 5 for example):
+5 4 3 2 1   
+4 3 2 1   
+3 2 1   
+2 1   
+1'''
 
     number:int = n
     for val in range(0, n):
@@ -22,3 +21,5 @@ def function1(n):
         n-=1
 
 function1(5)
+
+print(function1.__doc__)
