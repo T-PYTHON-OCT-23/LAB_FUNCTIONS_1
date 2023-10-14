1b1f2d26a0aea3a1211335f06d8bcd24b1e7baf5
AlenaziHazal <hazalalenazi@gmail.com>
2023-10-14 14:14:34 +0300

    lab is done by hazal

---
+++ function.py
@@ -0,0 +1,20 @@
+def function_display(num:int) -> int :
+    """This function take on parameters as integer number and reverse the parameter and print the output in list and decrese the number each time to give us the triangle shape.
+    Note: Logical opreation here is to take number and subtract 1 form the given number to make the shape look. \n
+    Like this:\n
+    00000 \n
+    0000 \n
+    000 \n
+    00 \n
+    0 \n
+    """
+    
+    for n in range(num,0,-1):
+        for r in range(num,0,-1):
+            print(r,end=" ")
+        print()
+        num -= 1
+    return None
+
+function_display(5)
+function_display()
