6dbf6ffee9aa34faa5d0bd3410b197a06df51828
oudy yousef <oudy.usef@gmail.com>
2023-10-15 09:34:45 +0300

    lab_functions

---
+++ lab_functions.py
@@ -0,0 +1,16 @@
+ 
+def descending_from_5_to_1():
+    """A function that prints numbers in descending order from 5 to 1 and subtracts a number on the next line"""
+num = int(input("give me range : "))
+count = range(num ,0,-1)
+
+
+for i in count :  
+    
+    for n in count:
+            print(n, end = ' ')
+            
+    print()
+    
+descending_from_5_to_1()
+    
