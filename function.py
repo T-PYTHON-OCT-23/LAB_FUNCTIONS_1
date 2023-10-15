1f24ed9e544909251a01d656a254ef95b38a395e
alya <alyamem80@gmail.com>
2023-10-15 08:17:09 +0300

    lapFuncions solve

---
+++ function.py
@@ -0,0 +1,33 @@
+
+
+def print_num(n):
+    for i in range(n, 0, -1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        print()
+
+num = input("enter intger number : ")
+
+if num.isdigit():  
+    num = int(num)
+    if num > 0:
+        print_num(num)
+   
+   
+   
+    print(print_num . __doc__ )
+      
+
+'''by Alya programmer'''
+
+
+
+
+
+
+
+      
+     
+  
+
+
